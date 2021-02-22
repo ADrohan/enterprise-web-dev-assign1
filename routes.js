4aae864ec72f26b33866bb441497bb204b5fe76f
aileendrohan <aileendrohan@gmail.com>
2021-02-22 15:26:07 +0000

    basic setup for app

---
+++ routes.js
@@ -0,0 +1,14 @@
+const Pois = require("./app/controllers/pois");
+
+module.exports = [
+  { method: "GET", path: "/", config: Pois.index },
+  {
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: "./public",
+      },
+    },
+  },
+];
