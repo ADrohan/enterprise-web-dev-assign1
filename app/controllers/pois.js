ae54391b2345155949e9006e59c98bbf265666fe
aileendrohan <aileendrohan@gmail.com>
2021-02-23 14:50:34 +0000

    added vision Hapi plugin and handlebars engine

--- app/controllers/pois.js
+++ app/controllers/pois.js
@@ -1,17 +1,20 @@
 const Pois = {
   index: {
     handler: function (request, h) {
-      return h.file("./app/views/main.html");
+      //return h.file("./app/views/main.hbs");
+      return h.view("main", { title: "Welcome to Point of Information" });
     },
   },
   signup: {
     handler: function (request, h) {
-      return h.file("./app/views/signup.html");
+      //return h.file("./app/views/signup.hbs");
+      return h.view("signup", { title: "Signup to Point of Information" });
     },
   },
   login: {
     handler: function (request, h) {
-      return h.file("./app/views/login.html");
+      //return h.file("./app/views/login.hbs");
+      return h.view("login", { title: "Login to Point of Information" });
     },
   },
 };
